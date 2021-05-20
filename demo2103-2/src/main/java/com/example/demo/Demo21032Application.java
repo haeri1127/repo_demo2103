00f70a407695d7b04e6ede7129c793a29a8e3c18
haeri1127 <chr1127@naver.com>
2021-05-20 11:14:09 +0900

    컴파일 에러 테스ㅡ입니다.

--- demo2103-2/src/main/java/com/example/demo/Demo21032Application.java
+++ demo2103-2/src/main/java/com/example/demo/Demo21032Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Demo21032Application {
-
+	//Test클래스
 	public static void main(String[] args) {
 		SpringApplication.run(Demo21032Application.class, args);
 	}
